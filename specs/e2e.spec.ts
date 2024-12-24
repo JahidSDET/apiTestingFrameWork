74ecdaebcfc12eceece6533e0db979ca2359a0af
jahidulislam <jahid_humanist@yahoo.com>
2024-12-24 00:38:21 -0500

    commit

---
+++ specs/e2e.spec.ts
@@ -0,0 +1,72 @@
+import controller from '../controller/brand.controller';
+import { test, expect } from '@playwright/test';
+
+var userId;
+
+// Sample for authentication
+test('Authentication using POST API', async ({ request }) => {
+    const credentials = {
+        "email": "eve.holt@reqres.in",
+        "password": "cityslicka"
+    };
+
+    const response = await request.post('https://reqres.in/api/login', {
+        data: credentials,
+        headers: { "ACCEPT": "application/JSON" }
+    });
+    
+    const responseJson = await response.json();
+    console.log(responseJson);
+    expect(response.status()).toBe(200);
+    expect(responseJson).toHaveProperty('token');
+});
+
+test('Get user Details using GET API', async ({ request }) => {
+    const response = await request.get('https://reqres.in/api/users?page=2');
+    var responseJson = await response.json();
+    console.log(responseJson);
+    expect(response.status()).toBe(200);
+    expect(responseJson.data[0].first_name).toBe('Michael');
+});
+
+test('Create User using POST API', async ({ request }) => {
+    var user = {
+        "name": "playwright",
+        "job": "udemy"
+    };
+
+    const response = await request.post('https://reqres.in/api/users', {
+        data: user,
+        headers: { "ACCEPT": "applications/JSON" }
+    });
+    var responseJson = await response.json();
+    expect(response.status()).toBe(201);
+    expect(responseJson.name).toBe(`${user.name}`);
+    expect(responseJson.job).toBe(`${user.job}`);
+    userId = responseJson.id;
+});
+
+test('Update User using PUT API', async ({ request }) => {
+    var user = {
+        "name": "automation",
+        "job": "course"
+    };
+
+    const response = await request.put('https://reqres.in/api/users/' + userId, {
+        data: user,
+        headers: { "ACCEPT": "applications/JSON" }
+    });
+    var responseJson = await response.json();
+    expect(response.status()).toBe(200);
+    expect(responseJson.name).toBe(`${user.name}`);
+    expect(responseJson.job).toBe(`${user.job}`);
+    userId = responseJson.id;
+});
+
+test('Delete User using DELETE API', async ({ request }) => {
+    const response = await request.delete('https://reqres.in/api/users/' + userId);
+    expect(response.status()).toBe(204);
+
+    const response2 = await request.get('https://reqres.in/api/users/' + userId);
+    console.log(await response2.json());
+});
